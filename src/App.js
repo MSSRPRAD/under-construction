5dc48b18119919cb49ae4ef1180baa726376ba1d
Malladi Pradyumna <98510624+MSSRPRAD@users.noreply.github.com>
2023-08-30 00:56:46 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import acmLogo from './assets/acm_logo.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app under-construction">
+      <h1>Under Construction</h1>
+      <p>This site is currently under construction.</p>
+      <p>ACM BITS Hyderabad Campus Chapter</p>
+      <p>
+        Meanwhile, please contact us at{' '}
+        <a href="mailto:acm.bphc@hyderabad.bits-pilani.ac.in">acm.bphc@hyderabad.bits-pilani.ac.in</a>.
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function About() {
+  return (
+    <div className="app">
+      <h1>About Us</h1>
+      <p>This is the ACM BITS Hyderabad Campus Chapter.</p>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="app">
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for doesn't exist.</p>
+    </div>
+  );
+}
+
+const routing = (
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+  </BrowserRouter>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <body>
+      {routing}
+    </body>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
